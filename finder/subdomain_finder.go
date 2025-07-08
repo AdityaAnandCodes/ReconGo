853dc7c40da979fda0398c1dfc027ca18dc88b8a
AdityaAnandCodes <adityaanandatwork276@gmail.com>
2025-07-08 10:57:39 +0530

    feat: add subdomain discovery functionality with a predefined wordlist

--- finder/subdomain_finder.go
+++ finder/subdomain_finder.go
@@ -1,1 +1,39 @@
 package finder
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func DiscoverSubdomains(baseDomain string, wordlistpath string){
+	file,err := os.Open(wordlistpath)
+	if err != nil {
+		fmt.Println("Error Fetching The Wordlist")
+		return
+	}
+	defer file.Close()
+	var wordlist []string
+	scanner := bufio.NewScanner(file)
+	
+	for scanner.Scan() {
+		word := strings.TrimSpace(scanner.Text())
+		if word != "" {
+			wordlist = append(wordlist, word)
+		}	
+	}
+	if len(wordlist) == 0 {
+		fmt.Println("Wordlist is Empty")
+		return
+	}
+
+	for _, domain := range wordlist {
+		subdomain := fmt.Sprintf("%s.%s", domain, baseDomain )
+		ips, err := net.LookupHost(subdomain)
+		if err == nil {
+			fmt.Printf("Found: %s → %v\n", subdomain , ips)
+		}
+	}
+}
